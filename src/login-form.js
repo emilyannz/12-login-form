26ac0be6bf83b17a352dc29c003e64ff7b01e873
Emily Znamierowski <emilyannz@gmail.com>
2016-03-23 19:50:09 -0500

    LoginForm listens for clicks complete

--- src/login-form.js
+++ src/login-form.js
@@ -3,20 +3,18 @@ export default class LoginForm {
   // "LoginForm" constructor
   constructor(form, email) {
     this.form = form;
-    this.userinfo = [{
-      username: `aaron@theironyard.com`, password: `password123`
-    }, {
-      username: `admin@google.com`,
-      password: `pandas`
-    }, {
-      username: email,
-      password: `honeycrisp`
-    }, ];
-    this.form.addEventListener(`submit`, (ev) => {
+    this.userinfo = [
+    { username: `aaron@theironyard.com`, password: `password123` },
+    { username: `admin@google.com`, password: `pandas` },
+    { username: email, password: `honeycrisp` },
+    ];
+    // LoginForm listens for clicks
+    this.form.addEventListener(`click`, (ev) => {
       ev.preventDefault();
       this.validateInputs();
     });
   }
+  // LoginForm validate
   validate(username, password) {
     return this.userinfo.reduce((prev, current) => {
       if (current.username === username && current.password === password) {
@@ -25,12 +23,12 @@ export default class LoginForm {
       return prev;
     }, false);
   }
-
+  // LoginForm validates inputes; find the values of the inputs in the form here
   validateInputs() {
     const emailInput = this.form.querySelector(`.login-form__email`).value;
     const passInput = this.form.querySelector(`.login-form__password`).value;
     const valmsg = this.form.querySelector(`.login-form__validation-message`);
-
+  // Run Validate
     if (this.validate(emailInput, passInput)) {
       valmsg.innerText = ``;
     } else {
